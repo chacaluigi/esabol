5ea5baabcf4073c83756f2884b44043cc2b50a06
chacaluigi <mariomachaca5477@gmail.com>
2026-02-12 22:32:52 -0400

    style: backend format structure

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,10 @@
-console.log('Hollaads');
+import express from 'express';
+import { PORT } from './config.js';
+import userRoutes from './routes/users.routes.js';
+
+const app = express();
+
+app.use(userRoutes);
+
+app.listen(PORT || 3000);
+console.log('Server on port', PORT);
---
+++ server/src/routes/users.routes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+const router = Router();
+
+router.get('/users', (req, res) => {
+  res.send('obteniendo usuarios');
+});
+
+export default router;
