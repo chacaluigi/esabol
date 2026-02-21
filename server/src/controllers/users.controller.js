e49ffcba4393e1bd44c9a067b6d9c2801b3c446a
chacaluigi <mariomachaca5477@gmail.com>
2026-02-20 20:46:42 -0400

    feat: add update and delete user for controllers

--- server/src/controllers/users.controller.js
+++ server/src/controllers/users.controller.js
@@ -28,36 +28,51 @@ export const getUser = async (req, res) => {
 export const createUser = async (req, res) => {
   try {
     const { name, email } = req.body;
+
     if (!name || !email)
       return res.status(400).json({ msg: 'Bad request. Empty fields' });
     const user = await User.create({ name, email });
-    res.json(user);
+
+    res.status(201).json(user);
   } catch (error) {
+    console.error(error);
+
     if (error.name === 'SequelizeUniqueConstraintError')
       return res.status(409).json({ msg: 'Email already exists' });
-    console.error(error);
     res.status(500).json({ msg: 'Internal server error' });
   }
 };
 
 export const updateUser = async (req, res) => {
   try {
-    const users = await User.findAll();
-    if (users.length === 0)
-      return res.status(404).json({ msg: 'Users not found' });
-    res.json(users);
+    const { id } = req.params;
+    const data = req.body;
+    const user = await User.findByPk(id);
+
+    if (!user) return res.status(404).json({ msg: 'User not found' });
+
+    user.set(data);
+    await user.save();
+
+    res.json(user);
   } catch (error) {
     console.error(error);
+    if (error.name === 'SequelizeUniqueConstraintError')
+      return res.status(409).json({ msg: 'Email already exists' });
+
     res.status(500).json({ msg: 'Internal server error' });
   }
 };
 
 export const deleteUser = async (req, res) => {
   try {
-    const users = await User.findAll();
-    if (users.length === 0)
-      return res.status(404).json({ msg: 'Users not found' });
-    res.json(users);
+    const { id } = req.params;
+    const user = await User.destroy({ where: { userId: id } });
+
+    if (user === 0)
+      return res.status(404).json({ msg: 'User does not exists.' });
+
+    res.sendStatus(204);
   } catch (error) {
     console.error(error);
     res.status(500).json({ msg: 'Internal server error' });
