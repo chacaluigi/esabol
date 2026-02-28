89c648206648bce99701966ffc584e323bf6630f
chacaluigi <mariomachaca5477@gmail.com>
2026-02-28 11:18:55 -0400

    docs: database define has been commented

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -6,7 +6,9 @@ export const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: 'postgres',
   define: {
+    //para cambiar los nombres de columnas de camelCase a snake_case en la bd
     underscored: true,
+    //en false para pluralizar nombres de tablas: User --> users
     freezeTableName: false,
     timestamps: true,
   },
