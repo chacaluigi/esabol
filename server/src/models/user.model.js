4e18439610e740f78b84a57dd6c13b061b2696df
chacaluigi <mariomachaca5477@gmail.com>
2026-03-03 20:44:36 -0400

    fix: add name for unique constraint to email and username on user  model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -14,12 +14,12 @@ const User = sequelize.define('User', {
   email: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true,
+    unique: 'user_email_unique',
   },
   username: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true,
+    unique: 'user_username_unique',
   },
   active: {
     type: DataTypes.BOOLEAN,
